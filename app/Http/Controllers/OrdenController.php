b4332a6d444e1a15f95cbcc75fe5a958c4df999f
Jorge Gomez <103078336+JorgeIGCZ@users.noreply.github.com>
2022-10-28 01:09:54 -0500

    Update OrdenController.php

--- app/Http/Controllers/OrdenController.php
+++ app/Http/Controllers/OrdenController.php
@@ -163,8 +163,6 @@ class OrdenController extends Controller
                  'WHERE G.Estatus = 1 AND G.Fecha_inicio <="'.$now.'" AND CP.Fecha_inicio <= "'.$now.'" AND AR.Alumno_id    = '.$alummoId.' ORDER BY Periodo_numero ASC '; 
 
         $periodos = DB::select($periodosQuery);
-        echo($now);
-        dd($alummoId);
 
         foreach($periodos as $periodo){
             $fechaInicioDate       = new DateTime($periodo->Fecha_inicio);
